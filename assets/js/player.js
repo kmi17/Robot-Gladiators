39864e86597ab186db8d9200d47a577de65dd918
camychhetri <chhetricamy@gmail.com>
2021-09-10 13:28:17 -0500

    initial game setup

---
+++ assets/js/player.js
@@ -0,0 +1,8 @@
+class Player {
+    constructor(name, health, attack, money) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.money = money;
+    }
+}
